0a138052799b146f4e42091276991b4fa378d61b
Erol444 <erol123444@gmail.com>
2024-06-03 11:38:15 +0200

    Updated version to 0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = readme_file.read_text(encoding='utf-8')
 
 setup(
     name="hubspot-api",
-    version="0.5.0",
+    version="0.6.0",
     description="Python library to interact with HubSpot's Private API",
     long_description=long_description,
     long_description_content_type='text/markdown',
